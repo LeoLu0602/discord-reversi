24ccc316e3fb462fffbfe112747bd0a8258b9676
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-04-17 19:43:49 -0400

    feat: add discordSdk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
+import { DiscordSDK } from '@discord/embedded-app-sdk';
+import { useEffect } from 'react';
+
+export default function App() {
+    useEffect(() => {
+        async function setUp() {
+            const discordSdk = new DiscordSDK(
+                import.meta.env.VITE_DISCORD_CLIENT_ID
+            );
+
+            try {
+                await discordSdk.ready();
+                console.log('Discord SDK is ready');
+            } catch (err) {}
+        }
+
+        setUp();
+    }, []);
+
     return (
         <>
             <h1 className="font-bold">Discord Reversi</h1>
         </>
     );
 }
-
-export default App;
