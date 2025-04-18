ff1ea84f1ab0bf09855c2dca790765f0622d2d78
Leo <96434243+LeoLu0602@users.noreply.github.com>
2025-04-17 21:54:14 -0400

    feat: show user info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { DiscordSDK } from '@discord/embedded-app-sdk';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function App() {
-    useEffect(() => {
-        let auth;
+    const [p1, setP1] = useState(null);
+    const [p2, setP2] = useState(null);
+    const [user, setUser] = useState(null);
 
+    useEffect(() => {
         async function setUp() {
             const discordSdk = new DiscordSDK(
                 import.meta.env.VITE_DISCORD_CLIENT_ID
@@ -33,7 +35,7 @@ export default function App() {
                 });
                 const { access_token } = await response.json();
 
-                auth = await discordSdk.commands.authenticate({
+                const auth = await discordSdk.commands.authenticate({
                     access_token,
                 });
 
@@ -41,7 +43,15 @@ export default function App() {
                     throw new Error('Authenticate command failed');
                 }
 
-                console.log(auth);
+                const { user } = auth;
+                const { id, username, global_name, avatar } = user;
+
+                setUser({
+                    id,
+                    username,
+                    global_name,
+                    avatar,
+                });
             } catch (err) {
                 console.error(err);
             }
@@ -52,7 +62,57 @@ export default function App() {
 
     return (
         <>
-            <h1 className="font-bold">Discord Reversi</h1>
+            <div className="w-full h-screen bg-[#302e2b] flex justify-center items-center">
+                <div className="h-screen aspect-square bg-emerald-500"></div>
+                <div className="absolute right-0 p-2 top-0 w-48 h-screen bg-[#262522] text-white flex-col gap-4 overflow-auto">
+                    {p1 ? (
+                        <div className="w-full h-24 p-2 bg-[#373633] flex items-center mb-2">
+                            <img
+                                className="h-full aspect-square"
+                                src={`https://cdn.discordapp.com/avatars/${p1.id}/${p1.avatar}.png`}
+                            />
+                            <div className="h-full grow px-2">
+                                <div>{p1?.global_name ?? 'Leo Lu'}</div>
+                                <div>1000</div>
+                            </div>
+                        </div>
+                    ) : (
+                        <button className="block w-full h-24 bg-[#373633] hover:bg-[#5b5954] text-4xl  cursor-pointer mb-2">
+                            &#43;
+                        </button>
+                    )}
+                    {p2 ? (
+                        <div className="w-full h-24 p-2 bg-[#373633] flex items-center mb-2">
+                            <img
+                                className="h-full aspect-square"
+                                src={`https://cdn.discordapp.com/avatars/${p2.id}/${p2.avatar}.png`}
+                            />
+                            <div className="h-full grow px-2">
+                                <div>{user?.global_name ?? 'Leo Lu'}</div>
+                                <div>1000</div>
+                            </div>
+                        </div>
+                    ) : (
+                        <button className="block w-full h-24 bg-[#373633] hover:bg-[#5b5954] text-4xl cursor-pointer mb-2">
+                            &#43;
+                        </button>
+                    )}
+                    <div className="w-full h-24 p-2 bg-[#373633] flex items-center mb-2">
+                        <img
+                            className="h-full aspect-square"
+                            src={
+                                user
+                                    ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
+                                    : '/discord-icon.png'
+                            }
+                        />
+                        <div className="h-full grow px-2">
+                            <div>{user?.global_name ?? 'Leo Lu'}</div>
+                            <div>1000</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
